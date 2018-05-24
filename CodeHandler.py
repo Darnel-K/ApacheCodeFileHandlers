30ddb76dc6b79f8f700b1b41f5a518354258a5b6
Darnel Kumar <darnel.kumar91@gmail.com>
2018-05-24 23:28:00 +0100

    -

--- CodeHandler.py
+++ CodeHandler.py
@@ -17,8 +17,7 @@ DOC = """<!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="CSS/Markdown.min.css">
-<!-- <link rel="stylesheet" href="//raw.githubusercontent.com/Darnel-K/Experimental-Code/master/Python%203/Markdown-Processor/CSS/Markdown.min.css"> -->
+<link rel="stylesheet" href="/CodeHandler/CSS/Markdown.min.css">
 </head>
 <body>
 """ + InnerHTML + """
