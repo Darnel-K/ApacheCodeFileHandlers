2c8ebfae17f284dadcac97ef724d6dae14a0bdb9
Darnel Kumar <darnel.kumar91@gmail.com>
2018-05-25 21:57:10 +0100

    -

--- CodeHandler.py
+++ CodeHandler.py
@@ -33,7 +33,7 @@ DOC.append('<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">')
 DOC.append('<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">')
 DOC.append('<link rel="stylesheet" href="/CodeHandler/CSS/MaterialLight.min.css">')
 DOC.append("</head>")
-DOC.append("<>")
+DOC.append("<body>")
 
 if (Heading is not None):
     InnerHTML.pop(0)
