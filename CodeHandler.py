fc97cc01a37d51f5b512572b968c076751048527
Darnel Kumar <darnel.kumar91@gmail.com>
2018-05-26 00:23:59 +0100

    -

--- CodeHandler.py
+++ CodeHandler.py
@@ -31,7 +31,7 @@ DOC.append("<html>")
 DOC.append("<head>")
 DOC.append('<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">')
 # DOC.append('<meta name="viewport" content="width=device-width, initial-scale=1.0">')
-DOC.append('<link rel="stylesheet" href="/CodeHandler/CSS/MaterialDark.min.css">')
+# DOC.append('<link rel="stylesheet" href="/CodeHandler/CSS/MaterialDark.min.css">')
 DOC.append("</head>")
 DOC.append("<body>")
 
