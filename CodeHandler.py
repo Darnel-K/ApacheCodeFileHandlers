83550247dc22b3cbedf59ea86e2d26eda0eb954c
Darnel Kumar <darnel.kumar91@gmail.com>
2018-05-25 19:40:08 +0100

    -

--- CodeHandler.py
+++ CodeHandler.py
@@ -19,7 +19,6 @@ DOC = """<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-<link rel="stylesheet" href="/CodeHandler/CSS/Native.css">
 <link rel="stylesheet" href="/CodeHandler/CSS/MaterialLight.min.css">
 </head>
 <body>
