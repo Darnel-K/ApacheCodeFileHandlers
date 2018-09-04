9e7b167fc4a885c5dbeb2c17d6de96cb01398b35
Darnel Kumar <darnel.kumar91@gmail.com>
2018-09-04 17:54:08 +0100

    -

--- CodeHandler.py
+++ CodeHandler.py
@@ -69,7 +69,7 @@ FileOutput = f.read()
 if ((FORMAT_QUERY not in URL_QUERY or (FORMAT_QUERY in URL_QUERY and (URL_QUERY[FORMAT_QUERY][0].lower() not in [1, "1", "yes", "y"]))) and FILE_EXTENSION not in ALWAYS_ACTIVE):
     mime = mimetypes.guess_type(FULL_FILE_NAME)
     if (mime[0] == None):
-        mime[0] = "text/html"
+        mime = ("text/html",)
     print("Content-type: " + mime[0] + ";charset=UTF-8\r\n\r\n")
     print(FileOutput)
 else:
