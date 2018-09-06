efa525e728d24d25f71559ffcc5fe7a0eb714797
Darnel Kumar <darnel.kumar91@gmail.com>
2018-09-06 23:19:13 +0100

    -

--- CodeHandler.py
+++ CodeHandler.py
@@ -15,7 +15,7 @@ from pygments.formatters import HtmlFormatter
 
 # cgitb.enable()
 
-ALWAYS_ACTIVE = [".md", ".txt", ".sql"]
+ALWAYS_ACTIVE = [".md", ".txt", ".sql", ".java", ".cs", ".py"]
 FORMAT_QUERY = "CH_FORMAT"
 EXTENSIONS = [
     'subscript',
@@ -69,10 +69,11 @@ def FormatFile(text, extension):
 FileOutput = f.read()
 if ((FORMAT_QUERY not in URL_QUERY or (FORMAT_QUERY in URL_QUERY and (URL_QUERY[FORMAT_QUERY][0].lower() not in [1, "1", "yes", "y"]))) and FILE_EXTENSION not in ALWAYS_ACTIVE):
     m = magic.Magic(mime=True)
-    mime = (m.from_file(FILENAME + FILE_EXTENSION), None)
+    # mime = (m.from_file(FILENAME + FILE_EXTENSION), None)
     # mime = mimetypes.guess_type(FULL_FILE_NAME, strict=False)
+    mime = (None,)
     if (mime[0] == None):
-        mime = ("text/html", None)
+        mime = ("application/octet-stream", None)
     print("Content-type: " + mime[0] + ";charset=UTF-8\r\n\r\n")
     # print(m.from_file(FILENAME + FILE_EXTENSION))
     print(FileOutput)
